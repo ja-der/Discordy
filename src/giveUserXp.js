9333b973c55c62138ff0b49b9382acd154374365
jder053@uottawa.ca <jder053@uottawa.ca>
2024-01-21 19:11:02 -0500

    started level system feature

---
+++ src/giveUserXp.js
@@ -0,0 +1,42 @@
+const { Client, Message } = require('discord.js');
+const Level = require('./Level');
+const calculateLevelXp = require('./calculateLevelXP');
+
+function getRandomXP(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(math.random() * (max - min + 1)) + min;
+}
+
+module.exports = async (client, message) => {
+    if (!message.inGuild() || message.author.bot) return;
+
+    const xpToGive = getRandomXP(5,15);
+
+    const query = {
+        userID: message.author.id,
+        guildID: message.guild.id,
+    };
+
+    try {
+        const level = await Level.findOne (query);
+
+        if (level) {
+            level.xp += xpToGive;
+
+            if (level.xp > calculateLevelXp(level.level)) {
+                level.xp = 0;
+                level.level += 1;
+
+                message.channel.send(`${message.member} you have levelled up to ** level ${level.level}**.`);
+            }
+
+            await level.save().catch((e) => {
+                console.log(`Error saving updated level ${e}`);
+                return;
+            })
+        }
+    } catch (error) {
+        console.log(`Error giving xp: ${error}`)
+    }
+}
